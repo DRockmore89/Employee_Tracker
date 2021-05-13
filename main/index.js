5ab8543caba6004cc6af1eac2e03712ebac4d568
DRockmore89 <donaldrockmore@yahoo.com>
2021-05-13 16:48:11 -0400

    change view function

--- main/index.js
+++ main/index.js
@@ -18,9 +18,68 @@ const firstQuestionFunction = () => {
         addDepartmentFunc();
      }else if (answer.role === "Add Roles") {
         //  fire the ask engineer question
-        addRolesfunction()
+        addRolesFunction()
      }else if (answer.role === "Add Employees") {
-        //  fire the 
+        //  fire the ask employees question
+        addEmployeesFunction()
+     }else if (answer.role === "View Departments") {
+        //  fire the ask intern questions
+        viewDepartmentsfunction();
+     }else if (answer.role === "View Roles") {
+        //  fire the assk inter question
+        viewRolesFunction();
+     }else if (answer.role === "ViewEmployees") {
+        //  fire the ask intern question
+        viewEmployeesFunction();
      }
+    });
+};
+
+firstQuestionFunction();
+
+function addDepartmentsFunction() {
+    inquirer.prompt(addDepartments).then((answer) => {
+        console.log(answers);
+        firstQuestionFunction();
+    });
+}
+
+function addRolesFunction() {
+    inquirer.prompt(addRoles).then((answer) => {
+        console.log(answer);
+        firstQuestionFunction();
+    });
+}
+
+function addEmployeesFunction() {
+    inquirer.prompt(addEmployees).then((answer) => {
+        console.log(answer);
+        firstQuestionFunction();
+    });
+}
+
+function viewDepartmentsFunction() {
+    
+        console.log(viewDepartments);
+        firstQuestionFunction();
     }
-)}
+   
+
+function viewRolesFunction() {
+        console.log(viewRoles);
+        firstQuestionFunction();
+    }   
+
+
+function viewEmployeesFunction() {
+        console.log(viewEmployees);
+        firstQuestionFunction();
+    }   
+
+
+function updateEmployeeRoleFunction() {
+    inquirer.prompt(updateEmployeeRole).then((answer) => {
+        console.log(answers);
+
+    })
+}
